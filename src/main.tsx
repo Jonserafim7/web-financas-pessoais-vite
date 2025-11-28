527f4f7c40df2b4f0855dde6b9c61105e8f6a220
jonas <jonserafim7@gmail.com>
2025-11-27 22:42:26 -0300

    Add Sentry integration for error tracking and performance monitoring
    Integrate Sentry into the application by importing the Sentry library and initializing it with the DSN for error tracking. Include examples for capturing exceptions and creating custom spans for performance monitoring in the documentation. Update package.json and package-lock.json to include the Sentry dependency.

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import * as Sentry from "@sentry/react";
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
@@ -7,6 +8,13 @@ import { useSession } from "./features/auth/hooks/use-session";
 import { ThemeProvider } from "./components/theme-provider";
 import { routeTree } from "./routeTree.gen";
 import { Spinner } from "./components/ui/spinner";
+
+Sentry.init({
+  dsn: "https://f6082a98ad580f26220840d8f63c370f@o4510440027324416.ingest.us.sentry.io/4510440028241920",
+  // Setting this option to true will send default PII data to Sentry.
+  // For example, automatic IP address collection on events
+  sendDefaultPii: true,
+});
 
 // Create a new router instance
 const router = createRouter({
